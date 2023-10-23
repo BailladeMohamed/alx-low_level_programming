1035518b8e9d4a75755337513ce4398a3982bdc9
BailladeMohamed <baillademohamed@gmail.com>
2023-10-23 14:26:41 -0700

    strchr task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,19 +4,16 @@
  * _strchr - Locates a character in a string
  * @st: string to search
  * @character: char to locate
- *
- * Return: Pointer to the first occurrence of @character in @st,
- *         or NULL if not found
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *st, char character)
 {
 	int i = 0;
 
-	while (st[i] != '\0')
+	for (; st[i] >= '\0'; i++)
 	{
 		if (st[i] == character)
 			return (&st[i]);
-		i++;
 	}
 
 	return (0);
